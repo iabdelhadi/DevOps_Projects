ff8519a32c47d78c9603c1878cd4b7673f2d8666
Abdelhadi Ait Ali <iabdelhadiaitali@gmail.com>
2024-10-11 11:24:59 +0100

    Added DevOps-Pipeline-PHP App

---
+++ DevOps-Pipeline-PHP/terraform/main.tf
@@ -0,0 +1,18 @@
+resource "aws_instance" "ubuntu-instance" {
+  ami           = var.ami
+  instance_type = "t2.micro"
+  key_name      = "jenkins-access-w"
+  security_groups = ["${aws_security_group.UbuntuSG.name}"]
+
+  tags  = {
+    Name  = "Ubuntu-EC2"
+    Owner  = "Ait Ali"
+  }
+
+  root_block_device {
+    volume_size = 30
+    volume_type = "gp2"
+    encrypted   = false
+  }
+}
+
---
+++ DevOps-Pipeline-PHP/terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  type    = string 
+  description = "Region added in tfvars"
+}
+
+variable "ami" {
+  type    = string 
+  description = "ami added in tfvars"
+}
